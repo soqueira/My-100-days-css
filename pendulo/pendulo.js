1ebc8b0d5805b3dc6c0a5908221287c97bd3dd16
soqueira <vitorsiqueria@gmail.com>
2019-06-04 13:59:42 -0300

    Code refactoring

--- pendulo/pendulo.js
+++ pendulo/pendulo.js
@@ -1,18 +1,16 @@
-let div = document.getElementsByClassName('center')[0];
-for(let i = 0; i< 70; i++){
-  div.innerHTML += '<div class="dots"></div>'
-}
-let dots = document.querySelectorAll('.dots');
-
-
-
-let duration = 60;
-let nDots = 70;
-let oscilations = 40;
-let i = 0;
-let time;
-  for(i; i <= nDots; i++){
-      time = duration / (oscilations + nDots - i);
-      timeAnim = time * 1000;
-      dots[i].style.cssText = 'animation: wave '+ timeAnim +'ms ease-in-out infinite';
+function Pendulum() {
+  let container = document.getElementsByClassName('center')[0];
+  let duration = 60;
+  let nDots = 70;
+  let oscilations = 40;
+  let time;
+  let timeAnim;
+  for (let i = 0; i < 70; i++) {
+    time = duration / (oscilations + nDots - i);
+    timeAnim = time * 1000;
+    container.innerHTML += '<div class="dots"></div>';
+    let dots = document.getElementsByClassName('dots')[i];
+    dots.style.cssText = 'animation: wave ' + timeAnim + 'ms ease-in-out infinite'
   }
+}
+Pendulum()
